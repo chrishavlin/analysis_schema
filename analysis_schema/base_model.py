73d57829c7e282931ff5045804933ee54f1a008d
chrishavlin <chris.havlin@gmail.com>
2022-02-03 11:58:08 -0600

    some helpful prep for an api endpoint

--- analysis_schema/base_model.py
+++ analysis_schema/base_model.py
@@ -4,25 +4,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-def show_plots(schema, files):
-    """
-    This function accepts the schema model and runs it using yt code which returns
-    a list. This function iterates through the list and displays each output.
-
-    Args:
-        schema ([dict]): the analysis schema filled out with yt specificaions
-    """
-    result = schema._run()
-    print(result)
-    for output in range(len(tuple(result))):
-        print("each output:", result[output])
-        if files == "Jupter":
-            result[output].show()
-        if files != "Jupyter":
-            result[output].save()
-            print("Files with output have been created!")
-
-
 class ytBaseModel(BaseModel):
     """
     A class to connect attributes and their values to yt operations and their
--- analysis_schema/run_analysis.py
+++ analysis_schema/run_analysis.py
@@ -1,28 +1,87 @@
 import argparse
 import json
 
-from .base_model import show_plots
 from .schema_model import ytModel
+from typing import Union, Optional
 
 
-def load_and_run(json_file, files):
+def show_plots(schema_result: list, image_type: str):
     """
-    A function to load the user JSON and load it into the analysis schema model, and
+    This function accepts results of a schema model run, which returns
+    a list. This function iterates through the list and displays each output.
+
+    Parameters
+    ----------
+    schema_result: list
+        the result of an analysis schema .run()
+    image_type: str
+        if "Jupyter" will call show, otherwise save
+    """
+
+    for output in schema_result:
+        if image_type == "Jupyter":
+            output.show()
+        if image_type != "Jupyter":
+            output.save()
+
+
+def load_and_run(
+    json_file: str, show_plots: Optional[bool] = True, image_type: Optional[str] = None
+):
+    """
+    A function to load the JSON file into the analysis schema model, and
     the run that model to produce an output.
 
-    Args:
-        json_file (json file): the JSON users edit
+    Parameters
+    ----------
+    json_file: str
+        the json file to load
+    show_plots: bool
+        if True, will either save or show any resulting plots, depending on
+        the value of the `files` parameter (default is True)
+    image_type: str
+        if show_plots is True, this string determines how the plot is returned.
+        If image_type=="Jupyter", the plot will be displayed with .show(),
+        otherwise it will be saved to file.
     """
+
     # open the file where the user is entering values
-    live_json = open(json_file)
-    # assign to a variable
-    live_schema = json.load(live_json)
-    live_json.close()
+    with open(json_file, "r") as live_json:
+        # assign to a variable
+        live_schema = json.load(live_json)
     # remove schema line
     live_schema.pop("$schema")
-    # create analysis schema model
-    analysis_model = ytModel(Data=live_schema["Data"], Plot=live_schema["Plot"])
-    print(show_plots(analysis_model, files))
+
+    result = validate_and_run(live_schema)
+
+    if show_plots:
+        show_plots(result, image_type)
+
+    return result
+
+
+def validate_and_run(json_contents: Union[str, dict]) -> list:
+    """
+    validates a json string or a python dict as a ytModel then executes
+    the model, returning results
+
+    Parameters
+    ----------
+    json_contents: Union[str, dict]
+        either a raw json string or a python dictionary containing json contents
+
+    Returns
+    -------
+    list
+        the results of running the analysis schema, stored in a list
+    """
+
+    # this is useful for an api server endpoint
+    if isinstance(json_contents, str):
+        analysis_model = ytModel.parse_raw(json_contents)
+    elif isinstance(json_contents, dict):
+        analysis_model = ytModel.parse_obj(json_contents)
+    return analysis_model._run()
 
 
 if __name__ == "__main__":
@@ -42,4 +101,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # run the analysis
-    load_and_run(args.JSONFile, args.ImageFormat)
+    _ = load_and_run(args.JSONFile, image_type=args.ImageFormat)
